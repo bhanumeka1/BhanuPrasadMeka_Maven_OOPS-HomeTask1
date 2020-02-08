658349ee114114a7883ff02d4cf2865322214097
bhanumeka1 <59662390+bhanumeka1@users.noreply.github.com>
2020-02-08 23:40:12 +0530

    Add files via upload

---
+++ HomeTask1/src/test/java/NewYearGift.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class NewYearGift {
+	public static void main(String args[])
+	{
+		 
+		Sweet s1 = new Sweet();
+		Sweet s2 = new Sweet();
+		Sweet.getweight();
+		Choclates c1,c2,c3;
+		c1 =new Candy();
+		c2 =new Candy();
+		c3 =new Candy();
+		Candy.sortbyweights();
+		Candy.sortbycosts();
+		
+	}
+}
